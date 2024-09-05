ce012536c9f98ead09d0c3e1727cd7f3e9704a78
arsenedo <arsenii.nedorezov@imperix.ch>
2024-09-05 17:53:57 +0200

    Added switch case for actions

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const express = require('express')
 const WebSocket = require('ws');
+const Player = require("./classes/player.js");
+const Game = require('./classes/game.js');
 const app = express()
 const port = 3000
 
 // Initialize WebSocket server
 const wss = new WebSocket.Server({ port: 8080 });
 
+const game = new Game();
+
 // WebSocket event handling
 wss.on('connection', (ws) => {
   console.log('A new client connected.');
 
   // Event listener for incoming messages
-  ws.on('message', (message) => {
-    console.log('Received message:', message.toString());
-
-    // Broadcast the message to all connected clients
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
-      }
-    });
+  ws.on('message', (e) => {
+    const data = JSON.parse(e);
+    switch (data.action) {
+      case "registerPlayer" :
+        const player = new Player(game.players.length, data.payload);
+        game.addPlayer(player)
+    
+        // Broadcast the message to all connected clients
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(game.players.length);
+          }
+        });
+        break;
+    }
   });
 
   // Event listener for client disconnection
